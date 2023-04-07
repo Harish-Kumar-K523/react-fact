35aaee5ef0e9e97f551c1583c957db8dcc5429e3
0xcryptolub <blackhat1523@gmail.com>
2023-04-07 07:59:16 +0530

    Site is created

--- Footer.js
+++
@@ -1,7 +0,0 @@
-export default function Footer(){
-    return(
-        <footer className="footer">
-                &copy; 2023 Made with ❤️ by <a href="https://github.com/0xcryptolub">0xcryptolub</a>
-        </footer>
-    )
-}
